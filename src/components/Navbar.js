2194fb82358f391c011fbe442478eacd964f5c8f
Joann Pan <joannlpan@gmail.com>
2022-01-13 17:52:14 -0500

    import icons into header component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './Navbar.css';
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
     return (
         <div className="wrapper">
+
             <div className="nav">
 
-           <div className="header">
-            <img src={require('../logos/logo2.png')} className="header"/>
-            </div>
+            <div className="header">
+                <img src={require('../logos/logo2.png')} className="header"/>
+                </div>
 
-            </div>
+                </div>
             
         </div>
     )

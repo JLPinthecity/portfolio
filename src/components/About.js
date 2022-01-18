8adbd91f353fef9552e4d450d2c35dfce1fedd23
Joann Pan <joannlpan@gmail.com>
2022-01-18 11:13:08 -0500

    install react router dom and wrap app in router

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,17 @@ import React from 'react'
 function About() {
     return (
         <div>
-            About Me 
+            About
 
+            Hi there! My name is Joann and I'm a classically trained journalist (went to J-school) and bootcamp-taught software engineer. After a decade of making content for various big lifestyle brands, I decided to pursue my dream of learning how to code. 
 
-            I'm a classically trained journalist with a curious mind. After a decade of making content for various big lifestyle brands, I decided to pursue my dream of learning how to code. As a graduate of Flatiron School's self-guided software engineering program, I have the skills to create beautiful websites.  
+            Like magic (with a side of hard work), I'm able to create dynamic mobile-first websites from scratch. 
+
+            As a developer, I thrive to write clean and accessible code--and maintain a can-do attitude. As a human, I strive to spread kindness and positivity. 
+
+            Likes: Harry Potter, Lord of the Rings, baking, hiking, reading.
         </div>
     )
 }
 
-export default About
+export default About;

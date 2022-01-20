41279688983c428c07e62a3c134f0468ecb9c666
Joann Pan <joannlpan@gmail.com>
2022-01-20 17:27:19 -0500

    add media query for smaller screens

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import {React, useState} from 'react';
-import '../App.css';
 import { Outlet } from "react-router-dom";
 import Header from './Header';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
 import MinimalistFooter from './MinimalistFooter';
+import '../App.css';
 
 function Layout() {
 

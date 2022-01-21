14c5b7fb504c9325dc936ed39649d9fbe614c8c7
Joann Pan <joannlpan@gmail.com>
2022-01-21 09:15:59 -0500

    add effects to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import Header from './Header';
-import MinimalistFooter from './MinimalistFooter';
-import Sidebar from './Sidebar';
+import './About.css';
 
 function About() {
     return (
-        <>
-          
+        <div className="about-wrapper">
             
-        
-            <div className="about-title">
-            ABOUT JOANN
-            </div>
+            <div className="about-first-row">
 
-            <div className="summary">
-                Hi there! My name is Joann and I'm a classically trained journalist (went to J-school) and bootcamp-taught software engineer. After a decade of making content for various big lifestyle brands, I decided to pursue my dream of learning how to code. 
-                <br/>
-                Like magic (with a side of hard work), I'm able to create dynamic mobile-first websites from scratch. 
-                <br/>
-                As a developer, I thrive to write clean and accessible code--and maintain a can-do attitude. As a human, I strive to spread kindness and positivity. 
+                <div className="about-title gradient-text">
+                about
                 </div>
-                
 
-                <div className="footer">
-        <MinimalistFooter/>
+                <br/>
+                <br/>
+
+                <div className="summary">
+                    Hi there! My name is Joann and I'm a classically trained journalist (went to J-school) and bootcamp-taught software engineer. After a decade of making content for various lifestyle brands, I decided to pursue my dream of learning how to code. 
+                    <br/>
+                    <br/>
+                    Open to: Opportunities to build beautiful, dynamic mobile-first websites from scratch. As a developer, I strive to write clean and accessible code. As a human, I like to spread kindness and positivity. 
+                </div>
+
+                </div>
+
+                    <br/>
+                    <br/>
+
+                <div className="block">
+                    <img src={require('../images/books.jpg')} className="illustration" alt="illustration of books"/>
+                
+                    <div className="title">
+                    experience & education
+                    </div>
+                </div>
+                    
+        
         </div>
-        </>
     )
 }
 

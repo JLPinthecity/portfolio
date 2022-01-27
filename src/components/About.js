860d4006148aeceb5da2c63747f7fec18cedd978
Joann Pan <joannlpan@gmail.com>
2022-01-27 15:55:20 -0500

    add more media queries and attempt to fix sidebar issue in mobile

--- src/components/About.js
+++ src/components/About.js
@@ -1,149 +1,145 @@
-import React from 'react';
-import laptop from '../images/laptop.svg';
-import books from '../images/books.svg';
-import peony from '../images/peony.svg';
+import React from "react";
+import laptop from "../images/laptop.svg";
+import books from "../images/books.svg";
+import peony from "../images/peony.svg";
 
-import './About.css';
+import "./About.css";
 
 function About() {
-    return (
-        <div className="about-wrapper">
-            
-            <div className="about-row">
+  return (
+    <div className="about-wrapper">
+      <div className="about-row">
+        <div className="about-title">about</div>
 
-                <div className="about-title">
-                about
-                </div>
+        <br />
+        <br />
 
-                <br/>
-                <br/>
-        
-                <div className="summary">
-                    Hi there! My name is Joann and I'm a classically trained journalist (went to J-school) and bootcamp-taught programmer. After a decade of making content for various lifestyle brands, I decided to pursue my dream of learning how to code. 
-                    <br/>
-                    <br/>
-                    Open to: Opportunities to build dynamic mobile-first websites from scratch. As a developer, I strive to write clean and accessible code. As a human, I like to spread kindness and positivity. 
-                </div>
-
-            </div>
-
-            <br/>
-            <br/>
-
-            <div className="block">
-
-                <div className="illustration-container">
-                    <img src={books} className="illustration books" alt="illustration of books"/>
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className="title">
-                experience & education
-                </div>
-
-                <div className="text">
-           
-                After gaining a master's degree in journalism from the NYU Arthur L. Carter Journalism Institute, I worked at Oprah.com as a web producer, creating content, writing newsletters, and developing content strategy. At Well+Good, I worked as an assistent branded content editor, conceptualizing and writing sponsored wellness-focused content.
-                <br/>
-                <br/>
-                I happily graduated from Flatiron School's Online Software Engineering Bootcamp in July 2021. 
-                </div>
-        
-            <br/>
-            <br/>
-
-                <div className="illustration-container">
-                    <img src={laptop} className="illustration macbook" alt="illustration of macbook air with keys and screen showing"/>
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <div className="title">
-                capabilities
-                </div>
-            
-                
-                <div className="technologies-container">
-                <div className="technologies text">
-
-                    <div class="technology">
-                    HTML
-                    </div>
-
-                    <div class="technology">
-                    CSS
-                    </div>
-
-                    <div class="technology">
-                    Ruby
-                    </div>
-                </div>
-
-                <div className="technologies text">
-                    <div class="technology">
-                    Sinatra
-                    </div>
-
-                    <div class="technology">
-                    Rails
-                    </div>
-
-                    <div class="technology">
-                    JavaScript
-                    </div>
-
-                </div>
-
-                <div className="technologies text">
-
-                    <div class="technology">
-                    React
-                    </div>
-
-                    <div class="technology">
-                    Redux
-                    </div>
-
-                    <div class="technology">
-                    Git
-                    </div>
-                </div>
-
-                </div>
-
-                <br/>
-                <br/>
-
-                <div className="title">
-                The Fun Stuff
-                </div>
-
-                <div className="text">
-                Off the web, you can find me watering my plants, walking my sweet dog Buffy, and planning my next dream trip (Oahu!). Nothing makes me happier than piles of books, meals with friends, and interior-design blogs. 
-                </div>
-
-                <br/>
-                <br/>
-                <br/>
-
-                <div className="illustration-container">
-                    <img src={peony} className="illustration peony" alt="illustration of pink peony flower"/>
-                </div>
-                <br/>
-                <br/>
-                <div className="prompt">
-                Want to build something together? Get in touch <a href="mailto:hitherejoann@gmail.com?subject=CONTACT%20FORM">here</a>
-                </div>
-
-                <br/>
-                <br/>
-                <br/>
-
-
-            </div>     
+        <div className="summary">
+          Hi there! My name is Joann and I'm a classically trained journalist
+          (went to J-school) and bootcamp-taught programmer. After a decade of
+          making content for various lifestyle brands, I decided to pursue my
+          dream of learning how to code.
+          <br />
+          <br />
+          Open to: Opportunities to build dynamic mobile-first websites from
+          scratch. As a developer, I strive to write clean and accessible code.
+          As a human, I like to spread kindness and positivity.
         </div>
-    )
+      </div>
+
+      <br />
+      <br />
+
+      <div className="block">
+        <div className="illustration-container">
+          <img
+            src={books}
+            className="illustration books"
+            alt="illustration of books"
+          />
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="title">
+          experience & education
+          <br />
+          <br />
+        </div>
+
+        <div className="text">
+          After gaining a master's degree in journalism from the NYU Arthur L.
+          Carter Journalism Institute, I worked at Oprah.com as a web producer,
+          creating content, writing newsletters, and developing content
+          strategy. At Well+Good, I worked as an assistent branded content
+          editor, conceptualizing and writing sponsored wellness-focused
+          content.
+          <br />
+          <br />I happily graduated from Flatiron School's Online Software
+          Engineering Bootcamp in July 2021.
+        </div>
+
+        <br />
+        <br />
+
+        <div className="illustration-container">
+          <img
+            src={laptop}
+            className="illustration macbook"
+            alt="illustration of macbook air with keys and screen showing"
+          />
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="title">capabilities</div>
+        <br />
+
+        <div className="technologies-container">
+          <div className="technologies text">
+            <div class="technology">HTML</div>
+
+            <div class="technology">CSS</div>
+
+            <div class="technology">Ruby</div>
+          </div>
+
+          <div className="technologies text">
+            <div class="technology">Sinatra</div>
+
+            <div class="technology">Rails</div>
+
+            <div class="technology">JavaScript</div>
+          </div>
+
+          <div className="technologies text">
+            <div class="technology">React</div>
+
+            <div class="technology">Redux</div>
+
+            <div class="technology">Git</div>
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="title">The Fun Stuff</div>
+        <br />
+
+        <div className="text">
+          Off the web, you can find me watering my plants, walking my sweet dog
+          Buffy, and planning my next dream trip (Oahu!). Nothing makes me
+          happier than piles of books, meals with friends, and interior-design
+          blogs.
+        </div>
+
+        <br />
+        <br />
+        <br />
+
+        <div className="illustration-container">
+          <img
+            src={peony}
+            className="illustration peony"
+            alt="illustration of pink peony flower"
+          />
+        </div>
+        <br />
+        <br />
+        <div className="prompt">
+          Want to build something together? Get in touch{" "}
+          <a href="mailto:hitherejoann@gmail.com?subject=CONTACT%20FORM">
+            here
+          </a>
+        </div>
+
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import { SidebarContent } from './SidebarContent';
+import React from "react";
+import { SidebarContent } from "./SidebarContent";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 function Sidebar(props) {
-
-    const menuItems = SidebarContent.map((item, key) => {
-        return (
-            <li key={key} className="item">
-                <div>
-                <Link to={item.link} onClick={props.clickMenuIcon}>{item.title}</Link>
-                </div>
-            </li>
-        )
-    })
-
-    let sidebarClass = 'sidebar_wrapper';
-    if (props.show) {
-        sidebarClass = 'sidebar_wrapper open'
-    }
-
+  const menuItems = SidebarContent.map((item, key) => {
     return (
-        <div className={sidebarClass}>
-
-            <div className="main">
-
-                <ul>
-                    {menuItems}
-                </ul>
-            
-            </div>
+      <li key={key} className="item">
+        <div>
+          <Link to={item.link} onClick={props.clickMenuIcon}>
+            {item.title}
+          </Link>
         </div>
-    )
+      </li>
+    );
+  });
+
+  let sidebarClass = "sidebar_wrapper";
+  if (props.show) {
+    sidebarClass = "sidebar_wrapper open";
+  }
+
+  return (
+    <div className={sidebarClass}>
+      <div className="main-container">
+        <ul>{menuItems}</ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
-

a78fd30e88724256e535ea53255b18828adc49c5
Joann Pan <joannlpan@gmail.com>
2022-05-15 11:02:02 -0400

    add minor changes and fix typo

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -1,62 +1,97 @@
-import React from 'react';
-import './Code.css'
-import './Writing.css'
+import React from "react";
+import "./Code.css";
+import "./Writing.css";
 
 function Writing() {
-    return (
-        <div className="writing-wrapper">
+  return (
+    <div className="writing-wrapper">
+      <div className="publication title">
+        Well+Good | branded content
+        <br />
+      </div>
 
-            <div className="publication title">
-                Well+Good | branded content
-                <br/>
-            </div>
+      <div className="clips text">
+        <a href="https://www.wellandgood.com/australian-natural-beauty-ingredients/">
+          <p>The newest class of skin-saving ingredients hails from…</p>
+        </a>
+        <a href="https://www.wellandgood.com/essential-oils-aura-cacia-positive-change/">
+          <p>How your essential oil habit can help support women </p>
+        </a>
+        <a href="https://www.wellandgood.com/best-running-cities-westin/">
+          <p>Why runcations are the cool new travel trend</p>
+        </a>
+        <a href="https://www.wellandgood.com/wellness-destinations-in-thailand/">
+          <p>
+            6 destinations in thailand that prove it’s the ultimate self-care
+            vacation spot
+          </p>
+        </a>
+        <br />
+        <br />
+      </div>
 
-            <div className="clips text">
-                <a href="https://www.wellandgood.com/australian-natural-beauty-ingredients/"><p>The newest class of skin-saving ingredients hails from…</p></a>
-                <a href="https://www.wellandgood.com/essential-oils-aura-cacia-positive-change/"><p>How your essential oil habit can help support women </p></a>
-                <a href="https://www.wellandgood.com/best-running-cities-westin/"><p>Why runcations are the cool new travel trend</p></a>
-                <a href="https://www.wellandgood.com/wellness-destinations-in-thailand/"><p>6 destinations in thailand that prove it’s the ultimate self-care vacation spot</p></a>
-                <br/><br/>
-            </div>
+      <div className="publication title">
+        Purple Carrot | lifestyle content
+        <br />
+      </div>
 
-            <div className="publication title">
-                Purple Carrot | lifestyle content
-                <br/>
-            </div>
+      <div className="clips text">
+        <a href="https://www.purplecarrot.com/blog/acupuncture-or-acupressure/">
+          <p>Acupuncture vs. acupressure: What you need to know</p>
+        </a>
+        <a href="https://www.purplecarrot.com/blog/cooking-with-a-food-processor/">
+          <p>What to make with your food processor</p>
+        </a>
+        <a href="https://www.purplecarrot.com/blog/vegan-travel-expert-tips/">
+          <p>
+            7 expert tips for maintaining a plant-based diet while traveling
+          </p>
+        </a>
+        <br />
+        <br />
+      </div>
 
-            <div className="clips text">
-                <a href="https://www.purplecarrot.com/blog/acupuncture-or-acupressure/"><p>Acupuncture vs. acupressure: What you need to know</p></a>
-                <a href="https://www.purplecarrot.com/blog/cooking-with-a-food-processor/"><p>What to make with your food processor</p></a>
-                <a href="https://www.purplecarrot.com/blog/vegan-travel-expert-tips/"><p>7 expert tips for maintaining a plant-based diet while traveling</p></a>
-                <br/><br/>
-            </div>
+      <div className="publication title">
+        Taste of Home | lifestyle content
+        <br />
+      </div>
 
-            <div className="publication title">
-                Taste of Home | lifestyle content
-                <br/>
-            </div>
+      <div className="clips text">
+        <a href="https://www.tasteofhome.com/article/what-size-air-fryer-do-i-need/">
+          <p>Find the air fryer size that's best for you</p>
+        </a>
+        <a href="https://www.tasteofhome.com/article/how-to-clean-small-appliances/">
+          <p>
+            The no-hassle way to deep-clean any electrical kitchen appliance{" "}
+          </p>
+        </a>
+        <a href="https://www.tasteofhome.com/article/vanishing-home-economics-class/">
+          <p>The vanishing home economics class and what will be lost</p>
+        </a>
+        <br />
+        <br />
+      </div>
 
-            <div className="clips text">
-                <a href="https://www.tasteofhome.com/article/what-size-air-fryer-do-i-need/"><p>Find the air fryer size that's best for you</p></a>
-                <a href="https://www.tasteofhome.com/article/how-to-clean-small-appliances/"><p>The no-hassle way to deep-clean any electrical kitchen appliance </p></a>
-                <a href="https://www.tasteofhome.com/article/vanishing-home-economics-class/"><p>The vanishing home economics class and what will be lost</p></a>
-                <br/><br/>
-            </div>
+      <div className="publication title">
+        Oprah.com | lifestyle content
+        <br />
+      </div>
 
-            <div className="publication title">
-                Oprah.com | lifestyle content
-                <br/>
-            </div>
-
-            <div className="clips text">
-                <a href="https://www.oprah.com/inspiration/difret-movie-interview"><p>5 things you must be willing to do to reach your dreams</p></a>
-                <a href="https://www.oprah.com/money/savings-secrets-from-people-who-have-paid-off-debt"><p>Savings Ssecrets from people who have overcome enormous debt</p></a>
-                <a href="https://www.oprah.com/home/how-to-brighten-a-dark-room"><p>5 easy ways to brighten up a dark apartment</p></a>
-                <br/><br/>
-            </div>
-            
-        </div>
-    )
+      <div className="clips text">
+        <a href="https://www.oprah.com/inspiration/difret-movie-interview">
+          <p>5 things you must be willing to do to reach your dreams</p>
+        </a>
+        <a href="https://www.oprah.com/money/savings-secrets-from-people-who-have-paid-off-debt">
+          <p>Savings secrets from people who have overcome enormous debt</p>
+        </a>
+        <a href="https://www.oprah.com/home/how-to-brighten-a-dark-room">
+          <p>5 easy ways to brighten up a dark apartment</p>
+        </a>
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default Writing;

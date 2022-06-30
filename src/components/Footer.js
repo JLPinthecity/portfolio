e145124b1344a08f3be64d526d504befad292f01
Joann Pan <joannlpan@gmail.com>
2022-06-30 12:43:48 -0400

    fix mobile footers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,59 @@
-import React from 'react'
-import { AiOutlineMail, AiOutlineLinkedin, AiOutlineGithub } from "react-icons/ai";
+import React from "react";
+import {
+  AiOutlineMail,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
 import { BsPinterest } from "react-icons/bs";
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer_wrapper">
+  return (
+    <div className="footer_container">
+      <div className="footer_main">
+        <div className="contact">
+          <h2>Contact</h2>
+        </div>
 
-            <div className="footer_container">
+        <div className="row">
+          <AiOutlineMail className="icon" />
 
-                <div className="contact">
-                    <h2>Contact</h2>
-                </div>
-            
-                <div className="row">
-                    <AiOutlineMail className="icon"/> 
-    
-                    <div className="email">hitherejoann@gmail.com</div>
-                </div>
+          <div className="email">hitherejoann@gmail.com</div>
+        </div>
 
-            <br/>
-            <br/>
+        <br />
+        <br />
 
-                <div className="social_icons">
-                    <a href="https://www.linkedin.com/in/joannpan" target="_blank"  rel="noreferrer"><AiOutlineLinkedin className="icon"/></a>
-                    <a href="https://www.pinterest.com/joannpan9/" target="_blank"  rel="noreferrer"><BsPinterest className="icon"/></a>
-                    <a href="https://github.com/JLPinthecity" target="_blank"  rel="noreferrer"><AiOutlineGithub className="icon"/></a>
-                </div>
+        <div className="social_icons">
+          <a
+            href="https://www.linkedin.com/in/joannpan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineLinkedin className="icon" />
+          </a>
+          <a
+            href="https://www.pinterest.com/joannpan9/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsPinterest className="icon" />
+          </a>
+          <a
+            href="https://github.com/JLPinthecity"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineGithub className="icon" />
+          </a>
+        </div>
 
-                <div className="copyright" >
-                    <h4>Copyright © 2022 Joann Pan</h4>
-                </div>
-
-            </div>
+        <div className="copyright">
+          <h4>Copyright © 2022 Joann Pan</h4>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default Footer;
